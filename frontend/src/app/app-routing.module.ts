4053e2d93421c13f24c02b9c600454585e34941e
nouamanelachhab <58518856+nouamanelachhab@users.noreply.github.com>
2020-12-16 14:51:24 +0100

    updated products page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+   
+  {
+    path: "",
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) 
+  },
+  { path: 'Products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
